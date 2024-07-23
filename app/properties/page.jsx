5dd27243d695236ee06d5dd6d17eba9e4856260c
Hasyeef Masud <70051252+mhasyeef@users.noreply.github.com>
2024-07-24 00:46:24 +0800

    refactored code

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,6 @@
 //import properties from "@/properties.json";
-import PropertyCard from "@/components/PropertyCard";
-import connectDB from "@/config/database";
-import Property from "@/models/Property";
 import PropertySearchForm from "@/components/PropertySearchForm";
+import Properties from "@/components/Properties";
 
 // async function fetchProperties() {
 //   try {
@@ -18,12 +16,6 @@ import PropertySearchForm from "@/components/PropertySearchForm";
 // }
 
 const PropertiesPage = async () => {
-  await connectDB();
-  const properties = await Property.find({});
-
-  //sort by date
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
   return (
     <>
       <section className="bg-blue-700 py-4">
@@ -34,20 +26,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
